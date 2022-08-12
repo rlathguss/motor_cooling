a22e37b184b00e0bbcafc2a5bec1000cf457c836
rlathguss <rlathgus0417@gmail.com>
2022-08-12 13:51:33 +0900

    cantrans v2

--- can/can.ino
+++ can/can.ino
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <string.h>
 #include "RF24.h"
-#include "SoftReset.h"
+//#include "SoftReset.h"
 
 #define slave1 0x205
 #define slave2 0x114
@@ -15,12 +15,13 @@ const float steinconstA = 0.001092768344245138;
 const float steinconstB = 0.000181450723833218;      
 const float steinconstC = 0.000000222855858126706000; 
 
+
 RF24 radio(9, 10); //CE, SS
 MCP2515 mcp2515(8);   //cs핀 번호 설정
 const byte CLK = 3;   // define CLK pin (any digital pin)
 const byte DIO = 4;   // define DIO pin (any digital pin)
-uint8_t address[6] = "04171";
-byte data[8] = {0x00 ,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
+uint8_t address[6] = "41715";
+byte data[8] = {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
 const float Diameter = 325.0;  //mm단위
 const uint8_t SEG_DONE[] = {
   SEG_B | SEG_C | SEG_D | SEG_E | SEG_G,           // d
@@ -56,193 +57,216 @@ TM1637Display display(CLK, DIO);// define dispaly object
 long RPM=0;
 int lin_vel=0;
 float sen11,sen22,sen33;
-uint16_t RPM_1,RPM_2,torque_buff,temp_buff,motor_vol,motor_curr, batt_vol,batt_curr,torque_demand,throttle_input_vol;
+uint16_t RPM_1,RPM_2,torque_buff,motor_temp,motor_vol,motor_curr, batt_vol,batt_curr,torque_demand,throttle_input_vol;
 uint8_t heatsink_temp;
+void temp(float sensor_value1,float sensor_value2,float sensor_value3);
+unsigned int mcp2515_receive1(byte recv[]);
+unsigned int mcp2515_receive2(byte recv[]);
+unsigned int mcp2515_receive3(byte recv[]);
 
 void setup() {
   display.setBrightness(0x0f);
   display.showNumberDec(9999);
-  Serial.begin(9600);
+  Serial.begin(115200);
   mcp2515.reset();
-  mcp2515.setBitrate(CAN_500KBPS);
+  mcp2515.setBitrate(CAN_1000KBPS);
   mcp2515.setNormalMode();
   radio.begin(); //아두이노-RF모듈간 통신라인
   radio.setPALevel(RF24_PA_MAX); 
   radio.openWritingPipe(address);
-  radio.openReadingPipe(0, address);
-  radio.stopListening(); //기본 :  송신모드
-
-  /////////////////////////////////////////Slv Start 0000////////////////////////////////////////
-  while(1){
-    if(radio.begin()){
-      int i1;
-      radio.startListening();  
-      radio.read(&i1,sizeof(i1));
-      //Serial.println(i1);
-      delay(10);
-        if(i1 == 257){
-          Serial.println("Slv started");
-          display.showNumberDec(000);
-          delay(300);
-          break;
-        }
-        if (i1 == 10280){ 
-          soft_restart();
-        }
-     }
-  }
-
-  /////////////////////////////////////////Radio check 1111//////////////////////////////////////
-  for(int i=0;i<30;i++){
-    if (radio.begin()){                        
-      display.showNumberDec(1111);
-      int c1 = 1;
-      radio.stopListening();
-      radio.write(&c1,sizeof(c1));
-      //Serial.println(c1);
-      delay(10);
-    }
-    else{
-      display.setSegments(SEG_ERR1);
-      radio.stopListening();
-      int c2 = 2;
-      radio.write(&c2,sizeof(c2));
-      //Serial.println(c2);
-      delay(10);
-    }
-  }
-
-  ///////////////////////////////////analogpin input check 2222//////////////////////////////////
-  for(int i=0;i<30;i++){
-    if(analogRead(A0)<1000&&analogRead(A1)<1000&&analogRead(A2)<1000){ 
-      display.showNumberDec(2222);
-      int c3 = 3;
-      radio.stopListening();
-      radio.write(&c3,sizeof(c3));
-      //Serial.println(c3);
-      delay(10);
-    }
-    else{
-      display.setSegments(SEG_ERR2);
-      int c4 = 4;
-      radio.stopListening();
-      radio.write(&c4,sizeof(c4));
-      //Serial.println(c4);
-      delay(10);
-    }
-  }
-
-  /////////////////////////////////////////Can Trans check 3333//////////////////////////////////
-  for(int i=0;i<30;i++){
-    if(digitalRead(2)==LOW||digitalRead(2)==HIGH){ 
-      display.showNumberDec(3333);
-      int c5= 5;
-      radio.stopListening();
-      radio.write(&c5,sizeof(c5));
-      //Serial.println(c5);
-      delay(10);
-    }
-    else{
-      display.setSegments(SEG_ERR3);
-      int c6 = 6;
-      radio.stopListening();
-      radio.write(&c6,sizeof(c6));
-      //Serial.println(c6);
-      delay(10);
-    }
-  }
-
-  /////////////////////////////////////////Record OR Reset///////////////////////////////////////
-  while(1){
-    if(radio.available()){
-      int i2;
-      radio.startListening();  
-      radio.read(&i2,sizeof(i2));
-      delay(10);
-        if(i2 == 2570){
-          Serial.println("Start recording");
-          display.setSegments(SEG_RECD);
-          delay(500);
-          break;
-        }
-        if (i2 == 10280){ 
-          soft_restart();
-        }
-     }
-  } 
+  radio.openReadingPipe(1, address);
+  radio.startListening(); //기본 :  송신모드
+//  digitalWrite(10,LOW);
+//  digitalWrite(8,HIGH);
+//  /////////////////////////////////////////Slv Start 0000////////////////////////////////////////
+//  while(1){
+//    if(radio.begin()){
+//      int i1;
+//      radio.startListening();  
+//      radio.read(&i1,sizeof(i1));
+//      delay(10);
+//      Serial.println(i1);
+//        if(i1 == 1){
+//          Serial.println("Slv started");
+//          display.showNumberDec(0000);
+//          delay(300);
+//          break;
+//        }
+//        if (i1 == 10280){ 
+//          //soft_restart();
+//        }
+//     }
+//  }
+//
+//  /////////////////////////////////////////Radio check 1111//////////////////////////////////////
+//  for(int i=0;i<30;i++){
+//    if (radio.begin()){                        
+//      display.showNumberDec(1111);
+//      int c1 = 1;
+//      radio.stopListening();
+//      radio.write(&c1,sizeof(c1));
+//      Serial.println(c1);
+//      delay(10);
+//    }
+//    else{
+//      display.setSegments(SEG_ERR1);
+//      radio.stopListening();
+//      int c2 = 2;
+//      radio.write(&c2,sizeof(c2));
+//      Serial.println(c2);
+//      delay(10);
+//    }
+//  }
+//
+//  ///////////////////////////////////analogpin input check 2222//////////////////////////////////
+//  for(int i=0;i<30;i++){
+//    if(analogRead(A0)<1000&&analogRead(A1)<1000&&analogRead(A2)<1000){ 
+//      display.showNumberDec(2222);
+//      int c3 = 3;
+//      radio.stopListening();
+//      radio.write(&c3,sizeof(c3));
+//      Serial.println(c3);
+//      delay(10);
+//    }
+//    else{
+//      display.setSegments(SEG_ERR2);
+//      int c4 = 4;
+//      radio.stopListening();
+//      radio.write(&c4,sizeof(c4));
+//      Serial.println(c4);
+//      delay(10);
+//    }
+//  }
+//
+//  /////////////////////////////////////////Can Trans check 3333//////////////////////////////////
+//  for(int i=0;i<30;i++){
+//    if(digitalRead(2)==LOW||digitalRead(2)==HIGH){ 
+//      display.showNumberDec(3333);
+//      int c5= 5;
+//      radio.stopListening();
+//      radio.write(&c5,sizeof(c5));
+//      Serial.println(c5);
+//      delay(10);
+//    }
+//    else{
+//      display.setSegments(SEG_ERR3);
+//      int c6 = 6;
+//      radio.stopListening();
+//      radio.write(&c6,sizeof(c6));
+//      Serial.println(c6);
+//      delay(10);
+//    }
+//  }
+//
+//  /////////////////////////////////////////Record OR Reset///////////////////////////////////////
+//  while(1){
+//    if(radio.available()){
+//      int i2;
+//      radio.startListening();  
+//      radio.read(&i2,sizeof(i2));
+//      Serial.println(i2);
+//      delay(10);
+//        if(i2 == 2570){
+//          Serial.println("Start recording");
+//          display.setSegments(SEG_RECD);
+//          delay(500);
+//          break;
+//        }
+//        if (i2 == 10280){ 
+//          //soft_restart();
+//        }
+//     }
+//  } 
+  delay(10);
 }
 
 
 
 void loop() {
   temp(analogRead(A0),analogRead(A1),analogRead(A2));
-
+  
   /////////////////////////////////////////Request slave1//////////////////////////////////////
   mcp2515_send(slave1,data);
 
   //Response slave1
-  byte recv[8];
-  unsigned int id = mcp2515_receive(recv);
+  byte recv1[8];
+  unsigned int id = mcp2515_receive1(recv1);
   if(id == -1){
     Serial.println("슬레이브1 오프라인!");
   }else{
-    RPM_1 = ((uint16_t)recv[1] << 8) | recv[0];
-	  RPM_2 = ((uint16_t)recv[3]<< 8) | recv[2];
-	  RPM = ((long)RPM_2 << 16) | RPM_1; // Motor RPM data 4bytes
-	  torque_buff = ((uint16_t)recv[5] << 8) | recv[4];// Motor Torque data 2bytes
-	  temp_buff = ((uint16_t)recv[7] << 8) | recv[6];// Motor Temperature 2bytes
+    RPM_1 = ((uint16_t)recv1[0] << 8) | recv1[1];
+	  RPM_2 = ((uint16_t)recv1[2]<< 8) | recv1[3];
+	  RPM = ((long)RPM_1 << 16) | RPM_2; // Motor RPM data 4bytes
+	  torque_buff = ((uint16_t)recv1[4] << 8) | recv1[5];// Motor Torque data 2bytes
+	  motor_temp = ((uint16_t)recv1[6] << 8) | recv1[7];// Motor Temperature 2bytes
     lin_vel = (Diameter) * RPM * PI * 60/1000000;   //KM/H
     display.showNumberDec(lin_vel);
-  }
+  }delay(10);
   
   /////////////////////////////////////////Request slave2//////////////////////////////////////
+  byte recv2[8];
   mcp2515_send(slave2,data);
 
   //Response slave2
-  id = mcp2515_receive(recv);
+  id = mcp2515_receive2(recv2);
   if(id == -1){
     Serial.println("슬레이브2 오프라인!");
   }else{
-    motor_vol= ((uint16_t)recv[1] << 8) | recv[0]; //Motor_vol data 2bytes
-	  motor_curr= ((uint16_t)recv[3] << 8) | recv[2]; //Motor_curr data 2bytes
-	  batt_vol= ((uint16_t)recv[5] << 8) | recv[4]; //Batt vol data 2bytes
-	  batt_curr= ((uint16_t)recv[7] << 8) | recv[6]; //Batt_curr data 2bytes
-  }
+    motor_vol= ((uint16_t)recv2[0] << 8) | recv2[1]; //Motor_vol data 2bytes
+	  motor_curr= ((uint16_t)recv2[2] << 8) | recv2[3]; //Motor_curr data 2bytes
+	  batt_vol= ((uint16_t)recv2[4] << 8) | recv2[5]; //Batt vol data 2bytes
+	  batt_curr= ((uint16_t)recv2[6] << 8) | recv2[7]; //Batt_curr data 2bytes
+  }delay(10);
 
   /////////////////////////////////////////Request slave3//////////////////////////////////////
+  byte recv3[8];
   mcp2515_send(slave3,data);
 
   //Response slave3
-  id = mcp2515_receive(recv);
+  id = mcp2515_receive3(recv3);
   if(id == -1){
     Serial.println("슬레이브3 오프라인!");
   }else{
-		torque_demand= ((uint16_t)recv[1]<< 8) | recv[0]; //torque_demand data 2bytes
-		throttle_input_vol= ((uint16_t)recv[3] << 8) | recv[2]; //throttle_input_vol data 2bytes
-		heatsink_temp= (uint8_t)recv[4]; // heatsink_temp data 1bytes
-  }
-
+		torque_demand= ((uint16_t)recv3[0]<< 8) | recv3[1]; //torque_demand data 2bytes
+		throttle_input_vol= ((uint16_t)recv3[2] << 8) | recv3[3]; //throttle_input_vol data 2bytes
+		heatsink_temp= (uint8_t)recv3[4]; // heatsink_temp data 1bytes
+  }delay(10);
 
   /////////////////////////////////////////Datapack RF Trans//////////////////////////////////////
-  float datapack[14];
-  datapack[0]=sen11; datapack[1]=sen22; datapack[2]=sen33; datapack[3]=RPM;
-  datapack[4]=lin_vel; datapack[5]=temp_buff; datapack[6]=heatsink_temp; datapack[7]= torque_buff;
-  datapack[8]= torque_demand; datapack[9]= motor_vol;datapack[10]= motor_curr; 
-  datapack[11]= batt_vol; datapack[12]= batt_curr; datapack[13]= throttle_input_vol;
+  float datapack[8],datapack1[8];
+  datapack[0]=4;
+  datapack[1]=sen11; datapack[2]=sen22; datapack[3]=sen33; datapack[4]=RPM;
+  datapack[5]=lin_vel; datapack[6]=motor_temp; datapack[7]=heatsink_temp; 
+  datapack1[0]=8;
+  datapack1[1]= torque_buff*0.1; datapack1[2]= torque_demand*0.1; datapack1[3]= motor_vol;
+  datapack1[4]= motor_curr; datapack1[5]= batt_vol; datapack1[6]= batt_curr; 
+  datapack1[7]= throttle_input_vol;
+
   radio.stopListening(); //송신모드
   radio.write(datapack,sizeof(datapack));
+  radio.write(datapack1,sizeof(datapack1));
+//  for(int i =0;i<7;i++){
+//    Serial.print(datapack[i],HEX);
+//    Serial.print(" ");}
+//  for(int i =0;i<7;i++){
+//    Serial.print(datapack1[i],HEX);
+//    Serial.print(" ");
+//    }
+//    Serial.println("");
+    delay(10);
 
-
+ 
   ///////////////////////////////////////////////Reset/////////////////////////////////////////////
-  radio.startListening();
-  if(radio.available()){
-  int i3;
-    radio.read(&i3,sizeof(i3));
-    if (i3 == 40){ 
-      soft_restart();
-    }
-  }
-  delay(200);
+//  radio.startListening();
+//  if(radio.available()){
+//  int i3;
+//    radio.read(&i3,sizeof(i3));
+//    if (i3 == 40){ 
+//      //soft_restart();
+//    }
+//  }
+//  digitalWrite(10,HIGH);
+//  digitalWrite(8,LOW);
 }
 
 
@@ -254,33 +278,81 @@ void mcp2515_send(unsigned int id, byte data[]){
     canMsg.data[i] = data[i];
   }
   mcp2515.sendMessage(&canMsg);
-  // Serial.println("[모터 컨트롤러에 보낸 메시지]");
-  // Serial.println(id,HEX);
-  // for (int i = 0; i<canMsg.can_dlc; i++)  {
-  //     Serial.print(canMsg.data[i],HEX);
-  //     Serial.print(" ");
-  // }
-  // Serial.println();
+   //Serial.println("[모터 컨트롤러에 보낸 메시지]");
+   //Serial.println(id,HEX);
+   for (int i = 0; i<canMsg.can_dlc; i++)  {
+    //Serial.print(canMsg.data[i],HEX);
+    //Serial.print(" ");
+   }
+   //Serial.println();
 }
 
-unsigned int mcp2515_receive(byte recv[]){
+/////////////////////////////////////////////////////////////receive1/////////////////////////////////////////
+unsigned int mcp2515_receive1(byte recv[]){
   struct can_frame canMsg;
   unsigned long t = millis();
   while(1){
-    if(millis() - t > 3000){
+    if(millis() - t > 2000){
       return -1;
       break;
     }
-    if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) {
+    if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK && canMsg.can_id == slave1) {
       for(int i =0;i<8;i++){
         recv[i] = canMsg.data[i];
+//        Serial.print(recv[i],HEX);
+//        Serial.print(" ");
       }
+      //Serial.println();
       return canMsg.can_id;
       break;
     }
   }
 }
 
+/////////////////////////////////////////////////////////////receive2/////////////////////////////////////////
+unsigned int mcp2515_receive2(byte recv[]){
+  struct can_frame canMsg;
+  unsigned long t = millis();
+  while(1){
+    if(millis() - t > 2000){
+      return -1;
+      break;
+    }
+    if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK&& canMsg.can_id == slave2) {
+      for(int i =0;i<8;i++){
+        recv[i] = canMsg.data[i];
+        //Serial.print(recv[i],HEX);
+        //Serial.print(" ");
+      }
+      //Serial.println();
+      return canMsg.can_id;
+      break;
+    }
+  }
+}
+
+/////////////////////////////////////////////////////////////receive3/////////////////////////////////////////
+unsigned int mcp2515_receive3(byte recv[]){
+  struct can_frame canMsg;
+  unsigned long t = millis();
+  while(1){
+    if(millis() - t > 2000){
+      return -1;
+      break;
+    }
+    if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK&& canMsg.can_id == slave3) {
+      for(int i =0;i<8;i++){
+        recv[i] = canMsg.data[i];
+        //Serial.print(recv[i],HEX);
+        //Serial.print(" ");
+      }
+      //Serial.println();
+      return canMsg.can_id;
+      break;
+    }
+  }
+}
+///////////////////////////////////////////////////////////////////////////temp_sensor//////////////////////////////////////////////////////////////
 void temp(float sensor_value1,float sensor_value2,float sensor_value3){
   sensor_value1 = (coolantsensorDivider*sensor_value1)/(1023-sensor_value1);
   sensor_value2 = (coolantsensorDivider*sensor_value2)/(1023-sensor_value2);
--- sensor_sd/NRF-MASTER/NRF-MASTER.ino
+++ sensor_sd/NRF-MASTER/NRF-MASTER.ino
@@ -1,138 +1,148 @@
 #include <SPI.h>
 #include "RF24.h"
 #include "SoftReset.h"
+#include <ArduinoJson.h>
 RF24 radio(9, 10); //CE, SS
-uint8_t address[6] = "04171";
+uint8_t address[6] = "41715";
+int nRecvMsg = 0; 
+bool is_datapack4 = false;
+bool is_datapack8 = false;
+
 
 void setup() {
-  Serial.begin(9600)
+  Serial.begin(115200);
   radio.begin(); 
   radio.setPALevel(RF24_PA_MAX); 
   radio.openWritingPipe(address);
-  radio.openReadingPipe(0, address);
+  radio.openReadingPipe(1, address);
   radio.startListening(); 
 
+
 /////////////////////////////////////////Slv Start 0000////////////////////////////////////////
-  String p1;
-  Serial.println("Start slv?");
-  while(1){
-    p1 = Serial.readString();
-    if(p1.indexOf("yes") != -1){
-      for(int i=0;i<30;i++){
-        radio.stopListening(); 
-        int i1 = 1;
-        radio.write(&i1,sizeof(i1));
-      }
-      break;
-    }
-    if(p1.indexOf("reset") != -1){
-    Serial.println("Restart the slv board ");
-      for(int i=0;i<30;i++){
-        int i3 = 40;
-        radio.stopListening();  
-        radio.write(&i3,sizeof(i3));
-        delay(10);
-      }
-    soft_restart();
-    }
-  }
-
-/////////////////////////////////////////Radio check 1111//////////////////////////////////////
-  while(1){
-    if(radio.begin()){
-      int c1;
-      radio.startListening();
-      radio.read(&c1,sizeof(c1));
-      //Serial.println(c1);
-      delay(10);
-        if (c1==257){ 
-          Serial.println("radio_connected");  
-          delay(500);              
-          break;
-        }
-    }
-    else{   
-      Serial.println("ERR_Radio");            
-      delay(500);
-      break;
-    }
-  }
-  
-///////////////////////////////////analogpin input check 2222//////////////////////////////////
-  while(1){
-    if(radio.begin()){
-      int c2;
-      radio.startListening();
-      radio.read(&c2,sizeof(c2));
-      delay(10);
-        if (c2==771){   
-          Serial.println("Analogsensor_connected"); 
-          delay(500);
-          break;
-        }
-        if (c2==1028){   
-          Serial.println("ERR_Analogsensor");            
-          delay(500);
-          break;
-        }
-    }
-  }
-
-/////////////////////////////////////////Can Trans check 3333//////////////////////////////////
-  while(1){
-    if(radio.begin()){
-      int c3;
-      radio.startListening();
-      radio.read(&c3,sizeof(c3));
-      //Serial.println(c3);  
-      delay(10);
-        if (c3==1285){   
-         Serial.println("CanTrans_connected");
-         delay(500);
-         break;
-        }
-        if (c3==1542){   
-          Serial.println("ERR_Digitalsensor");            
-          delay(500);
-          break;
-        }
-    }
-  }
-
-/////////////////////////////////////////Record OR Reset///////////////////////////////////////
-  String p2;
-  Serial.println("Start recording? ");
-  while(1){
-    p2 = Serial.readString();
-    if(p2.indexOf("yes") != -1){
-      for(int i=0;i<30;i++){
-        radio.stopListening(); //송신모드
-        int i2 = 10;
-        radio.write(&i2,sizeof(i2));
-      }
-      Serial.println("Senor1, Senor2, Senor3, RPM, Lin_vel, Motor_temp, Heatsink_temp, Motor_torque, Torque_demand, Motor_vol, Motor_curr, Batt_vol, Batt_curr, Throttle_input_vol"); // legend
-      break;
-    }
-
-    if(p1.indexOf("reset") != -1){
-      Serial.println("Restart the slv board ");
-      for(int i=0;i<30;i++){
-        int i3 = 40;
-        radio.stopListening();  
-        radio.write(&i3,sizeof(i3));
-        delay(10);
-      }
-    soft_restart();
-    }
-  }
+//  String p1;
+//  Serial.println("Start slv?");
+//  while(1){
+//    p1 = Serial.readString();
+//    if(p1.indexOf("yes") != -1){
+//      for(int i=0;i<30;i++){
+//        radio.stopListening(); 
+//        int i1 = 1;
+//        radio.write(&i1,sizeof(i1));
+//        Serial.println(i1);
+//        delay(10);
+//      }
+//      break;
+//    }
+//    if(p1.indexOf("reset") != -1){
+//    Serial.println("Restart the slv board ");
+//      for(int i=0;i<30;i++){
+//        int i3 = 40;
+//        radio.stopListening();  
+//        radio.write(&i3,sizeof(i3));
+//        delay(10);
+//      }
+//    soft_restart();
+//    }
+//  }
+//
+///////////////////////////////////////////Radio check 1111//////////////////////////////////////
+//  while(1){
+//    if(radio.begin()){
+//      int c1;
+//      radio.startListening();
+//      radio.read(&c1,sizeof(c1));
+//      Serial.println(c1);
+//      delay(10);
+//        if (c1==257){ 
+//          Serial.println("radio_connected");  
+//          delay(500);              
+//          break;
+//        }
+//    }
+//    else{   
+//      Serial.println("ERR_Radio");            
+//      delay(500);
+//      break;
+//    }
+//  }
+//  
+/////////////////////////////////////analogpin input check 2222//////////////////////////////////
+//  while(1){
+//    if(radio.begin()){
+//      int c2;
+//      radio.startListening();
+//      radio.read(&c2,sizeof(c2));
+//      Serial.println(c2);
+//      delay(10);
+//        if (c2==771){   
+//          Serial.println("Analogsensor_connected"); 
+//          delay(500);
+//          break;
+//        }
+//        if (c2==1028){   
+//          Serial.println("ERR_Analogsensor");            
+//          delay(500);
+//          break;
+//        }
+//    }
+//  }
+//
+///////////////////////////////////////////Can Trans check 3333//////////////////////////////////
+//  while(1){
+//    if(radio.begin()){
+//      int c3;
+//      radio.startListening();
+//      radio.read(&c3,sizeof(c3));
+//      Serial.println(c3);  
+//      delay(10);
+//        if (c3==1285){   
+//         Serial.println("CanTrans_connected");
+//         delay(500);
+//         break;
+//        }
+//        if (c3==1542){   
+//          Serial.println("ERR_Digitalsensor");            
+//          delay(500);
+//          break;
+//        }
+//    }
+//  }
+//
+///////////////////////////////////////////Record OR Reset///////////////////////////////////////
+//  String p2;
+//  Serial.println("Start recording? ");
+//  while(1){
+//    p2 = Serial.readString();
+//    if(p2.indexOf("yes") != -1){
+//      for(int i=0;i<30;i++){
+//        radio.stopListening(); //송신모드
+//        int i2 = 10;
+//        radio.write(&i2,sizeof(i2));
+//        Serial.println(i2);
+//      }
+//      Serial.println("Senor1, Senor2, Senor3, RPM, Lin_vel, Motor_temp, Heatsink_temp, Motor_torque, Torque_demand, Motor_vol, Motor_curr, Batt_vol, Batt_curr, Throttle_input_vol"); // legend
+//      break;
+//    }
+//
+//    if(p1.indexOf("reset") != -1){
+//      Serial.println("Restart the slv board ");
+//      for(int i=0;i<30;i++){
+//        int i3 = 40;
+//        radio.stopListening();  
+//        radio.write(&i3,sizeof(i3));
+//        delay(10);
+//      }
+//    soft_restart();
+//    }
+//  }
+Serial.println("Senor1, Senor2, Senor3, RPM, Lin_vel, Motor_temp, Heatsink_temp, Motor_torque, Torque_demand, Motor_vol, Motor_curr, Batt_vol, Batt_curr, Throttle_input_vol"); // legend
 }
 
 
 
 void loop() {
-
+  
 ////////////////////////////////////////Datapack RF Trans//////////////////////////////////////
-  radio.startListening(); 
   bool is_timeout = false;
   unsigned long t = millis();
   while(true){
@@ -148,18 +158,42 @@ void loop() {
     Serial.println("통신시간 초과!");
   }
   else{
-    float datapack[14];
+    float datapack[8],datalog[14];
     radio.read(datapack,sizeof(datapack));
-    Serial.print(datapack[0]);Serial.print("  ");Serial.print(datapack[1]);Serial.print("  ");
-    Serial.print(datapack[2]);Serial.print("  ");Serial.print(datapack[3]);Serial.print("  ");
-    Serial.print(datapack[4]);Serial.print("  ");Serial.print(datapack[5]);Serial.print("  ");
-    Serial.print(datapack[6]);Serial.print("  ");Serial.print(datapack[7]);Serial.print("  ");
-    Serial.print(datapack[8]);Serial.print("  ");Serial.print(datapack[9]);Serial.print("  ");
-    Serial.print(datapack[10]);Serial.print("  ");Serial.print(datapack[11]);Serial.print("  ");
-    Serial.print(datapack[12]);Serial.print("  ");Serial.print(datapack[13]);Serial.print("  ");
-    Serial.println(datapack[14]);
+    if (datapack[0]==4){
+      datalog[0]=datapack[1];
+      datalog[1]=datapack[2];
+      datalog[2]=datapack[3];
+      datalog[3]=datapack[4];
+      datalog[4]=datapack[5];
+      datalog[5]=datapack[6];
+      datalog[6]=datapack[7];
+      is_datapack4 = true;
+    }
+    if (datapack[0]==8){
+      datalog[7]=datapack[1];
+      datalog[8]=datapack[2];
+      datalog[9]=datapack[3];
+      datalog[10]=datapack[4];
+      datalog[11]=datapack[5];
+      datalog[12]=datapack[6];
+      datalog[13]=datapack[7];
+      is_datapack8 = true;
+    }
+    if(is_datapack4&&is_datapack8){
+      Serial.print(datalog[0]);Serial.print("  ");Serial.print(datalog[1]);Serial.print("  ");
+      Serial.print(datalog[2]);Serial.print("  ");Serial.print(datalog[3]);Serial.print("  ");
+      Serial.print(datalog[4]);Serial.print("  ");Serial.print(datalog[5]);Serial.print("  ");
+      Serial.print(datalog[6]);Serial.print("  ");Serial.print(datalog[7]);Serial.print("  ");
+      Serial.print(datalog[8]);Serial.print("  ");Serial.print(datalog[9]);Serial.print("  ");
+      Serial.print(datalog[10]);Serial.print("  ");Serial.print(datalog[11]);Serial.print("  ");
+      Serial.print(datalog[12]);Serial.print("  ");Serial.println(datalog[13]);
+      is_datapack4 = false; is_datapack8 = false;
+    }
   }
+  delay(10);
 
+ 
 ///////////////////////////////////////////////Reset/////////////////////////////////////////////
   String p3;
   if(Serial.available()){
@@ -176,3 +210,33 @@ void loop() {
     }
   }
 }
+
+
+//String output;
+//StaticJsonDocument<128> doc;
+//
+//doc["Sensor1"] = datapack[0];
+//doc["Sensor2"] = datapack[1];
+//doc["Sensor3"] = datapack[2];
+//doc["RPM"] = datapack[3];
+//doc["Lin_vel"] = datapack[4];
+//doc["Motor_temp"] = datapack[5];
+//doc["Heatsink_temp"] = datapack[6];
+//doc["Motor_torque"] = datapack1[0];
+//doc["Torque_demand"] = datapack1[1];
+//doc["Motor_vol"] = datapack1[2];
+//doc["Motor_curr"] = datapack1[3];
+//doc["Batt_vol"] = datapack1[4];
+//doc["Batt_curr"] = datapack1[5];
+//doc["Throttle_input_vol"] = datapack1[6];
+//
+//serializeJson(doc, output);
+//Serial.println(output);
+
+
+
+
+
+
+//Serial.println("Senor1, Senor2, Senor3, RPM, Lin_vel, Motor_temp, Heatsink_temp, Motor_torque, Torque_demand, Motor_vol, Motor_curr, Batt_vol, Batt_curr, Throttle_input_vol");
+//{"Sensor1":123,"Sensor2":123,"Sensor3":123,"RPM":123,"Lin_vel":123,"Motor_temp":123,"Heatsink_temp":123,"Motor_torque":123,"Torque_demand":123,"Motor_vol":123,"Motor_curr":123,"Batt_vol":123,"Batt_curr":123,"Throttle_input_vol":123,}
